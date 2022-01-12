2c28c822ff481669d7474e5ff00892d89982345d
tockn <tockn.jp@gmail.com>
2022-01-13 03:07:39 +0900

    :bug:

--- src/fastfire_collection.ts
+++ src/fastfire_collection.ts
@@ -1,8 +1,6 @@
 import { FastFireDocument } from './fastfire_document';
 
-export const FastFireCollection = (
-  collectionRef: string
-): PropertyDecorator => {
+export const FastFireCollection = (collectionRef: string): ClassDecorator => {
   return target => {
     FastFireDocument.collectionRefMap[target.constructor.name] = collectionRef;
   };
